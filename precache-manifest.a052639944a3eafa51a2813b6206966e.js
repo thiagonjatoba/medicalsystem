b92c76ad6880379de95ec3733be030357bc4f415
Thiago Nascimento Jatobá <thiago.jatoba@gmail.com>
2019-04-04 22:10:17 -0300

    Updates

---
+++ precache-manifest.a052639944a3eafa51a2813b6206966e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7408be3c584c398103c0adc6dfb71d35",
+    "url": "/medicalsystem/static/media/logo.7408be3c.png"
+  },
+  {
+    "revision": "126c36bed0fa1d1aa2d0",
+    "url": "/medicalsystem/static/js/runtime~main.5fe38454.js"
+  },
+  {
+    "revision": "c3e633bfcb86b3ac8420",
+    "url": "/medicalsystem/static/js/main.f8bdde06.chunk.js"
+  },
+  {
+    "revision": "134c56a17f4ef98ecde5",
+    "url": "/medicalsystem/static/js/2.64ce3f1e.chunk.js"
+  },
+  {
+    "revision": "c3e633bfcb86b3ac8420",
+    "url": "/medicalsystem/static/css/main.90b36b9e.chunk.css"
+  },
+  {
+    "revision": "134c56a17f4ef98ecde5",
+    "url": "/medicalsystem/static/css/2.c9f91b27.chunk.css"
+  },
+  {
+    "revision": "e0709ea75e4c75e91ea835a23d29ea6e",
+    "url": "/medicalsystem/index.html"
+  }
+];
